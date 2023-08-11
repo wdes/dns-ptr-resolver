3762030c6701a1eafdc811f0d58b69f0b8fe111e
William Desportes <williamdes@wdes.fr>
2023-08-11 12:31:15 +0200

    __INIT__

---
+++ README.md
@@ -0,0 +1,37 @@
+# dns-ptr-resolver
+
+A Rust program to resolve IP lists to their DNS PTR
+
+## Example input
+
+```text
+192.0.2.255
+::1
+1.1.1.1
+1.0.0.1
+2606:4700:4700::1111
+2606:4700:4700::1001
+8.8.8.8
+8.8.4.4
+9.9.9.9
+```
+
+## Example output
+
+```text
+1.0.0.1 # one.one.one.one.
+::1
+2606:4700:4700::1111 # one.one.one.one.
+1.1.1.1 # one.one.one.one.
+9.9.9.9 # dns9.quad9.net.
+8.8.4.4 # dns.google.
+2606:4700:4700::1001 # one.one.one.one.
+8.8.8.8 # dns.google.
+192.0.2.255
+```
+
+## Rebuild example
+
+```sh
+cargo run --release ./example/ips-to-resolve.txt > ./example/ips-resolved.txt
+```
---
+++ src/main.rs
@@ -0,0 +1,72 @@
+use trust_dns_client::client::{ Client, SyncClient };
+use trust_dns_client::udp::UdpClientConnection;
+use std::str::FromStr;
+use std::net::IpAddr;
+use trust_dns_client::op::DnsResponse;
+use trust_dns_client::rr::{ DNSClass, Name, RData, Record, RecordType };
+use rustdns::util::reverse;
+use rayon::prelude::*;
+use std::fs::read_to_string;
+use std::env;
+use std::process;
+
+fn get_ptr(conn: UdpClientConnection, addr: IpAddr) {
+    let client = SyncClient::new(conn);
+    // Specify the name, note the final '.' which specifies it's an FQDN
+    let name = Name::from_str(&reverse(addr)).unwrap();
+
+    let response: DnsResponse = client.query(&name, DNSClass::IN, RecordType::PTR).unwrap();
+    let answers: &[Record] = response.answers();
+
+    if answers.len() == 0 {
+        println!("{}", addr);
+        return;
+    }
+
+    if let Some(RData::PTR(ref res)) = answers[0].data() {
+        println!("{} # {}", addr, res);
+    } else {
+        assert!(false, "unexpected result")
+    }
+}
+
+fn resolve_file(filename: &str, dns_server: &str) {
+    let mut ips = vec![];
+    for line in read_to_string(filename).unwrap().lines() {
+        ips.push(IpAddr::from_str(line).unwrap());
+    }
+
+    rayon::ThreadPoolBuilder::new().num_threads(50).build_global().unwrap();
+    let address = dns_server.parse().unwrap();
+    let conn = UdpClientConnection::new(address).unwrap();
+
+    ips.into_par_iter()
+        .enumerate()
+        .for_each(|(_i, addr)| {
+            get_ptr(conn, addr);
+        });
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Use: ptr-resolver ./ips.txt");
+        process::exit(1);
+    }
+    resolve_file(&args[1], "1.1.1.1:53")
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_reverse_dns() {
+        assert_eq!(reverse("192.0.2.12".parse().unwrap()), "12.2.0.192.in-addr.arpa.");
+    }
+
+    #[test]
+    fn test_resolve_file() {
+        resolve_file("./example/ips-resolved.txt", "1.1.1.1:53");
+    }
+}
